4d673a97cf7fb49bebfcee1e0617084e7743d4de
andro675 <aykhanv@crocusoft.com>
2020-08-14 16:12:41 +0400

    project finished (5)

--- app/src/main/java/az/rabita/lifestep/ui/fragment/donation/DonationViewHolder.kt
+++ app/src/main/java/az/rabita/lifestep/ui/fragment/donation/DonationViewHolder.kt
@@ -1,19 +1,32 @@
 package az.rabita.lifestep.ui.fragment.donation
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import az.rabita.lifestep.R
 import az.rabita.lifestep.databinding.ItemComingToYouBinding
 import az.rabita.lifestep.pojo.apiPOJO.entity.Assocation
 
 class DonationViewHolder(private val binding: ItemComingToYouBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     fun bind(
         donationItemPOJO: Assocation,
         clickListener: (assocationId: String) -> Unit
-    ) = with(binding) {
+    ): Unit = with(binding) {
         data = donationItemPOJO
+        val percent = donationItemPOJO.percent
+        root.context?.let {
+            progressBar.progressDrawable = it.getDrawable(
+                when (percent) {
+                    in 0..30 -> R.drawable.progress_bar_rounded_low
+                    in 31..70 -> R.drawable.progress_bar_rounded_medium
+                    else -> R.drawable.progress_bar_rounded_high
+                }
+            )
+        }
         root.setOnClickListener { clickListener(donationItemPOJO.id) }
     }
 
